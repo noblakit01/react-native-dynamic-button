1e7c639ae0524f96f2b7d3b6b2e7ffe95ad10bc8
luan <luantm@dekiconnect.com>
2017-04-02 21:22:31 +0700

    Test create Play Shape

--- index.android.js
+++ index.android.js
@@ -12,20 +12,20 @@ import {
   View
 } from 'react-native';
 
+import {
+  Surface,
+  Shape,
+  Path
+} from 'ReactNativeART';
+
 export default class DynamicButton extends Component {
   render() {
+    const d = Path().moveTo(80, 20).lineTo(280, 150).lineTo(80, 280).close();
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Surface width="300" height="300">
+          <Shape stroke="#ff0000" strokeWidth="8" strokeJoin="round" strokeCap="round" d = {d} />
+        </Surface>
       </View>
     );
   }
@@ -37,17 +37,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('DynamicButton', () => DynamicButton);
