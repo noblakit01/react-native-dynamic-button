5507cb81c1757ed3d81acfa569fc0727315c2606
Tran Minh Luan <minhluan.phoenix@outlook.com>
2017-04-03 17:04:29 +0700

    Add Style enum

--- DynamicButton.js
+++ DynamicButton.js
@@ -8,20 +8,41 @@ import {
   Path
 } from 'ReactNativeART';
 
+export DynamicButtonType = {
+	'Play': 'play',
+	'Pause': 'pause',
+	'Stop': 'stop',
+};
+
 export class DynamicButton extends Component {
-  render() {
-    const width = this.props.style.width;
+	
+  currentPath() {
+	const width = this.props.style.width;
     const height = this.props.style.height;
     const padding = this.props.padding;
-    const d = Path()
-    .moveTo(width / 3 - padding, padding)
-    .lineTo(width - padding, height / 2)
-    .lineTo(width / 3 - padding, height - padding)
-    .close();
+    const type = this.props.type;
+    if (type == DynamicButtonType.Play) {
+	  const d = Path()
+		.moveTo(width / 3 - padding, padding)
+		.lineTo(width - padding, height / 2)
+		.lineTo(width / 3 - padding, height - padding)
+		.close();
+	  return d;
+	} else if (type == DynamicButtonType.Pause) {
+	  const d = Path()
+		.moveTo(width / 3 - padding, padding)
+		.lineTo(width - padding, height / 2)
+		.lineTo(width / 3 - padding, height - padding)
+		.close();
+	  return d;
+	}	
+  }	
+  	
+  render() {
     return (
       <View>
         <Surface width={width} height={height}>
-          <Shape stroke={this.props.strokeColor} strokeWidth={this.props.strokeWidth} d = {d} />
+          <Shape stroke={this.props.strokeColor} strokeWidth={this.props.strokeWidth} d = {this.state.d} />
         </Surface>
       </View>
     );
@@ -32,10 +53,12 @@ DynamicButton.propTypes = {
   strokeColor: PropTypes.string,
   strokeWidth: PropTypes.number,
   padding: PropTypes.number,
+  type: PropTypes.oneOf([DynamicButtonType.Play, DynamicButtonType.Pause, DynamicButtonType.Stop]),
 };
 
 DynamicButton.defaultProps = {
   strokeColor: '#9FABFF',
   strokeWidth: 8,
   padding: 40,
+  type: {DynamicButtonType.Play},
 };
